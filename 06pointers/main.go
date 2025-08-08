5f7000d049a09b74bf748646f2ffc547f53a537f
Debgandhar Ghosh <debg48@localhost.localdomain>
2025-08-08 12:47:12 +0530

    06pointers updated

--- 06pointers/main.go
+++ 06pointers/main.go
@@ -4,4 +4,16 @@ import "fmt"
 
 func main() {
 	fmt.Println("Welcome to Pointer Playground")
+	// var ptr *int
+	// fmt.Println("Value of Pointer is ",ptr)
+	// fmt.Printf("Type of Pointer is : %T\n",ptr)
+	myNumber := 23
+	var ptr  = &myNumber
+	fmt.Println("Value of Pointer is ",ptr)
+	fmt.Println("Value of Pointer is ",*ptr)
+	fmt.Printf("Type of Pointer is : %T\n",ptr)
+
+	*ptr = *ptr + 2
+	fmt.Println("The new value of variable is : ",myNumber)
+
 }
