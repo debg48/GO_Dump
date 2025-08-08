68f7927672a112567b1546d7407f5b78ed35f19c
Debgandhar Ghosh <debg48@localhost.localdomain>
2025-08-08 18:13:12 +0530

    07arrays completed

--- 07arrays/main.go
+++ 07arrays/main.go
@@ -1,7 +1,20 @@
 package main
 
-func main()  {
-	
+import "fmt"
+
+func main() {
+
+	fmt.Println("Welcome to Arrays Playground")
+
+	var arr [4]string
+	arr[0] = "apple"
+	arr[1] = "banana"
+	// arr[2] = "orange"
+	arr[3] = "grapes"
+
+	fmt.Println("The fruit list is : ", arr)
+	fmt.Println("The length of array is : ", len(arr))
+
+	var vegList = [3]string{"potato", "beans", "mushroom"}
+	fmt.Println("The veg list is : ", vegList)
 }
-
-
